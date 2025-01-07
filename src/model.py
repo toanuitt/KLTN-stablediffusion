dda50ebec8e431ac585f95c02cd7d5bcda1b3158
cancogang69 <90518328+Cancogang69@users.noreply.github.com>
2025-01-07 20:22:19 +0700

    debug

--- src/model.py
+++ src/model.py
@@ -5,34 +5,35 @@ from .networks import define_G
 
 class Pix2PixModel:
     def __init__(self, opt):
-        self.rank = opt.rank
+        self.rank = opt["rank"]
         self.device = (
             torch.device(f"cuda:{self.rank}")
             if self.rank is not None
             else torch.device("cpu")
         )
 
+        self.input_nc = opt.input_nc
         if "expand" in opt.extra_info:
-            opt.input_nc = opt.input_nc + 1
+            self.input_nc += 1
         if "label" in opt.extra_info:
-            opt.input_nc = opt.input_nc + 1
+            self.input_nc += 1
 
         self.generator = define_G(
-            opt.input_nc,
-            opt.output_nc,
-            opt.ngf,
-            opt.netG,
-            opt.norm,
-            opt.dropout,
-            output_type=opt.output_type,
-            unet_backbone=opt.unet_backbone,
+            self.input_nc,
+            opt["output_nc"],
+            opt["ngf"],
+            opt["netG"],
+            opt["norm"],
+            opt["dropout"],
+            output_type=opt["output_type"],
+            unet_backbone=opt["unet_backbone"],
         )
 
         assert os.path.exists(
-            opt.generator_path
+            opt["generator_path"]
         ), f"Cannot find {opt.generator_path}"
-        if opt.generator_path is not None:
-            self.load_network(opt.generator_path)
+        if opt["generator_path"] is not None:
+            self.load_network(opt["generator_path"])
 
     def load_network(self, generator_path, weights_only=True):
         print("loading the model from %s" % generator_path)
