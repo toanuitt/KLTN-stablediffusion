f2dab43d08baf9e25ce07e0e0a51a5dee56d2e2a
cancogang69 <90518328+Cancogang69@users.noreply.github.com>
2025-01-09 20:40:25 +0700

    debug

--- app.py
+++ app.py
@@ -118,6 +118,7 @@ def process_image_mask(
     cv2.imwrite("img_filled.png", image_filled)
     cv2.imwrite("object_image.png", object_image.astype(np.uint8))
 
+    object_image = utils.resize(object_image, unet_input_shape)
     if opts["sd"]["ip_adapter_id"] is None:
         object_images = []
     else:
