f8504010b6d48bbfc9db599fe4316d50565b491e
cancogang69 <90518328+Cancogang69@users.noreply.github.com>
2025-01-09 20:53:20 +0700

    debug

--- src/utils.py
+++ src/utils.py
@@ -313,15 +313,15 @@ def get_sd_pipeline(pipeline_opts):
         )
 
     ip_adapter_id = pipeline_opts["ip_adapter_id"]
-    if ip_adapter_id is not None:
-        pipe.load_ip_adapter(
-            "h94/IP-Adapter",
-            subfolder="models",
-            weight_name=[ip_adapter_id],
-            image_encoder_folder="models/image_encoder",
-            torch_dtype=torch.float16,
-        )
-        pipe.set_ip_adapter_scale(0.5)
+    # if ip_adapter_id is not None:
+    #     pipe.load_ip_adapter(
+    #         "h94/IP-Adapter",
+    #         subfolder="models",
+    #         weight_name=[ip_adapter_id],
+    #         image_encoder_folder="models/image_encoder",
+    #         torch_dtype=torch.float16,
+    #     )
+    #     pipe.set_ip_adapter_scale(0.5)
 
     return pipe
 
