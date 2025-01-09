c9998a75be77ff8b046039b29574787bf9a9b8a8
Toan <ductoan.uit@gmail.com>
2025-01-09 12:00:39 +0700

    Feat: add default value and change step

--- src/button.py
+++ src/button.py
@@ -2,13 +2,15 @@ import gradio as gr
 def create_control_elements():
     expand_direction = gr.Radio(
         label="Direction to expand image", 
-        choices=["Left", "Right"]
+        choices=["Left", "Right"],
+        value="left",
     )
     expand_pixels = gr.Number(
         label="Number of pixels to expand",
         minimum=0,
         maximum=1024,
         precision=0,
+        value=200,
     )
     prompt = gr.Textbox(
         label="Prompt", 
@@ -22,12 +24,14 @@ def create_control_elements():
         minimum=1,
         maximum=100,
         value=30,
+        step=1,
         label="Number of Inference Steps",
     )
     guidance_scale = gr.Slider(
         minimum=1.0, 
         maximum=20.0, 
         value=7.5, 
+        step=0.5,
         label="Guidance Scale"
     )
     denoise_strength = gr.Slider(
